53b17aff2b2666134eda2bed5d6bec29201330c5
David Warren <dwarren@teaglu.com>
2023-02-28 16:34:34 -0500

    Finish implementing Cloudflare
    Fixed problems with apex record.
    Implemented missing query logic for bootstrap.

--- src/com/teaglu/dnsalias/dns/cloudflare/CloudflareDnsZone.java
+++ src/com/teaglu/dnsalias/dns/cloudflare/CloudflareDnsZone.java
@@ -1,6 +1,8 @@
 package com.teaglu.dnsalias.dns.cloudflare;
 
 import java.io.IOException;
+import java.net.Inet4Address;
+import java.net.InetAddress;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -9,6 +11,9 @@ import java.util.TreeMap;
 
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.google.gson.JsonObject;
 import com.teaglu.composite.Composite;
 import com.teaglu.composite.exception.SchemaException;
@@ -17,8 +22,11 @@ import com.teaglu.dnsalias.dns.DnsRecordType;
 import com.teaglu.dnsalias.dns.DnsZone;
 import com.teaglu.dnsalias.dns.exception.DnsApiException;
 import com.teaglu.dnsalias.dns.exception.DnsException;
+import com.teaglu.dnsalias.dns.record.ARecord;
 
 public class CloudflareDnsZone implements DnsZone {
+	private static final Logger log= LoggerFactory.getLogger(CloudflareDnsZone.class);
+	
 	private @NonNull CloudflareAccount account;
 	private @NonNull String zoneName;
 	private @NonNull String zoneId;
@@ -61,9 +69,68 @@ public class CloudflareDnsZone implements DnsZone {
 			@NonNull String name,
 			@NonNull DnsRecordType type) throws DnsException, IOException
 	{
+		String searchName= name + "." + zoneName;
+		if (name.isBlank()) {
+			searchName= zoneName;
+		}
+		
+		String searchType= type.toString();
+		
 		List<@NonNull DnsRecord> rval= new ArrayList<>(4);
 		
-		// FIXME implement
+		// Run a query to get all the matching record IDs
+		Map<@NonNull String, @Nullable String> search= new TreeMap<>();
+		search.put("name", searchName);
+		search.put("type", searchType);
+		Iterable<@NonNull Composite> results=
+				account.query("/zones/" + zoneId + "/dns_records", search);
+		
+		// For A records
+		List<@NonNull Inet4Address> v4Addresses= new ArrayList<>(4);
+		
+		// For AAAA records
+		//List<Inet6Address> v6Addresses= new ArrayList<>(4);
+		
+		// For A / AAAA records
+		Integer ttl= null;
+		
+		for (Composite record : results) {
+			try {
+				String recordName= record.getRequiredString("name");
+				String recordType= record.getRequiredString("type");
+				
+				if (recordName.equalsIgnoreCase(searchName)) {
+					if (recordType.equalsIgnoreCase(searchType)) {
+						if (type == DnsRecordType.A) {
+							// Bundle all A records into one at the end
+							String recordContent= record.getRequiredString("content");
+							try {
+								InetAddress address= InetAddress.getByName(recordContent);
+								
+								if (address instanceof Inet4Address) {
+									v4Addresses.add((Inet4Address)address);
+								}
+							} catch (NumberFormatException formatException) {
+								log.warn("Ignoring unparseable address " + recordContent);
+							}
+							
+							if (ttl == null) {
+								ttl= record.getOptionalInteger("ttl");
+							}
+						} else {
+							// FIXME
+						}
+					}
+				}
+			} catch (SchemaException schemaException) {
+				throw new DnsApiException(
+						"Error reading record ID from response", schemaException);
+			}
+		}
+
+		if (type == DnsRecordType.A) {
+			rval.add(ARecord.Create(searchName, v4Addresses, ttl));
+		}
 		
 		return rval;
 	}
@@ -73,18 +140,46 @@ public class CloudflareDnsZone implements DnsZone {
 			@NonNull DnsRecord record,
 			boolean overwrite) throws DnsException, IOException
 	{
-		// FIXME deal with overwrite
+		if (overwrite) {
+			// Cloudflare doesn't really have the concept of a change set, but at some point
+			// we might look at doing the delete after the create
+			deleteRecord(record.getName(), record.getType());
+		}
+
+		String createName= record.getName();
+		if (createName.isBlank()) {
+			createName= "@";
+		}
+
+		String createType= record.getType().toString();
+				
+		Integer ttl= record.getPositiveTtl();
+		if (ttl == null) {
+			ttl= 1;
+		} else {
+			// The API will fail if you try to pass a TTL <60 or >86400
+			if (ttl < 60) {
+				log.warn("Requested record TTL for " + createName + " is " + ttl +
+						" seconds, but clamping to Cloudflare minimum of 60.");
+
+				ttl= 60;			
+			} else if (ttl > 86400) {
+				log.warn("Requested record TTL for " + createName + " is " + ttl +
+						" seconds, but clamping to Cloudflare maximum of 86400.");
+				
+				ttl= 86400;
+			}
+		}
 		
-		for (String value : record.getValues()) {
+		for (String value : record.getValues()) {			
 			JsonObject request= new JsonObject();
-			request.addProperty("name", record.getName());
-			request.addProperty("type", record.getType().toString());
+			request.addProperty("name", createName);
+			request.addProperty("type", createType);
 			request.addProperty("content", value);
-			
-			Integer ttl= record.getPositiveTtl();
+
 			
 			// 1 = automatic a/p docs
-			request.addProperty("ttl", ttl != null ? ttl : 1);
+			request.addProperty("ttl", ttl);
 			
 			account.create("/zones/" + zoneId + "/dns_records", request);
 		}
@@ -98,6 +193,9 @@ public class CloudflareDnsZone implements DnsZone {
 		// Cloudflare seems to take the relative name in the create call, but looks for the
 		// full name without a trailing dot in the query call
 		String searchName= name + "." + zoneName;
+		if (name.isBlank()) {
+			searchName= zoneName;
+		}
 		
 		// Run a query to get all the matching record IDs
 		Map<@NonNull String, @Nullable String> search= new TreeMap<>();
